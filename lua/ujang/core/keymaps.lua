3ed55912866f22ae40c0c57cdcd99627a6f1cd51
90169795 <90169795@bri.co.id>
2026-02-22 13:38:37 +0700

    edit config keymaps

--- lua/ujang/core/keymaps.lua
+++ lua/ujang/core/keymaps.lua
@@ -1,23 +1,22 @@
 vim.g.mapleader = " "
-
-local keymap = vim.keymap -- for conciseness
+local keymap = vim.keymap
 
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
--- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+-- increment/decrement
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+-- tab management (ganti prefix ke <leader>b)
+keymap.set("n", "<leader>bo", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>bx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>bn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "<leader>bp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>bf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
--- lua/ujang/plugins/toggleterm.lua
+++ lua/ujang/plugins/toggleterm.lua
@@ -4,7 +4,6 @@ return {
   event = "VeryLazy",
   config = function()
     local toggleterm = require("toggleterm")
-
     toggleterm.setup({
       size = function(term)
         if term.direction == "horizontal" then
@@ -13,19 +12,20 @@ return {
           return vim.o.columns * 0.4
         end
       end,
-      open_mapping = [[<C-_>]], -- toggle dengan Ctrl+\
+      open_mapping = [[<C-_>]],
       hide_numbers = true,
       shade_terminals = true,
       shading_factor = 2,
       start_in_insert = true,
       insert_mappings = true,
+      terminal_mappings = true,
       persist_size = true,
-      direction = "float", -- default: float | horizontal | vertical | tab
+      direction = "float",
       close_on_exit = true,
       shell = vim.o.shell,
       auto_scroll = true,
       float_opts = {
-        border = "curved", -- single | double | shadow | curved
+        border = "curved",
         winblend = 0,
         highlights = {
           border = "Normal",
@@ -34,34 +34,37 @@ return {
       },
     })
 
-    -- Keluar dari terminal mode
     local keymap = vim.keymap
+
+    -- Exit terminal mode
     keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
     keymap.set("t", "jk", "<C-\\><C-n>", { desc = "Exit terminal mode with jk" })
 
-    -- Toggle berbagai arah
-    keymap.set("n", "<leader>tf", "<cmd>ToggleTerm direction=float<CR>", { desc = "Toggle floating terminal" })
-    keymap.set("n", "<leader>th", "<cmd>ToggleTerm direction=horizontal<CR>", { desc = "Toggle horizontal terminal" })
-    keymap.set("n", "<leader>tv", "<cmd>ToggleTerm direction=vertical<CR>", { desc = "Toggle vertical terminal" })
-    keymap.set("n", "<leader>tt", "<cmd>ToggleTerm direction=tab<CR>", { desc = "Toggle terminal in new tab" })
+    -- Fallback Ctrl+/
+    keymap.set("n", "<C-_>", "<cmd>ToggleTerm<CR>", { desc = "Toggle terminal" })
+    keymap.set("t", "<C-_>", "<cmd>ToggleTerm<CR>", { desc = "Toggle terminal" })
+    keymap.set("i", "<C-_>", "<cmd>ToggleTerm<CR>", { desc = "Toggle terminal" })
 
-    -- Multiple terminal (ganti angka untuk terminal berbeda)
-    keymap.set("n", "<leader>t1", "<cmd>1ToggleTerm direction=float<CR>", { desc = "Terminal 1" })
-    keymap.set("n", "<leader>t2", "<cmd>2ToggleTerm direction=float<CR>", { desc = "Terminal 2" })
-    keymap.set("n", "<leader>t3", "<cmd>3ToggleTerm direction=float<CR>", { desc = "Terminal 3" })
+    -- Toggle arah (prefix <leader>tm)
+    keymap.set("n", "<leader>tmf", "<cmd>ToggleTerm direction=float<CR>", { desc = "Terminal float" })
+    keymap.set("n", "<leader>tmh", "<cmd>ToggleTerm direction=horizontal<CR>", { desc = "Terminal horizontal" })
+    keymap.set("n", "<leader>tmv", "<cmd>ToggleTerm direction=vertical<CR>", { desc = "Terminal vertical" })
+    keymap.set("n", "<leader>tmt", "<cmd>ToggleTerm direction=tab<CR>", { desc = "Terminal tab" })
 
-    -- Lazygit di dalam toggleterm (bonus!)
+    -- Multiple terminal
+    keymap.set("n", "<leader>tm1", "<cmd>1ToggleTerm direction=float<CR>", { desc = "Terminal 1" })
+    keymap.set("n", "<leader>tm2", "<cmd>2ToggleTerm direction=float<CR>", { desc = "Terminal 2" })
+    keymap.set("n", "<leader>tm3", "<cmd>3ToggleTerm direction=float<CR>", { desc = "Terminal 3" })
+
+    -- Lazygit
     local Terminal = require("toggleterm.terminal").Terminal
     local lazygit = Terminal:new({
       cmd = "lazygit",
       dir = "git_dir",
       direction = "float",
-      float_opts = {
-        border = "curved",
-      },
+      float_opts = { border = "curved" },
       on_open = function(term)
         vim.cmd("startinsert!")
-        -- Nonaktifkan Esc di lazygit supaya tidak konflik
         vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<Esc>", "<Esc>", { noremap = true, silent = true })
       end,
       on_close = function()
@@ -71,6 +74,6 @@ return {
 
     keymap.set("n", "<leader>lg", function()
       lazygit:toggle()
-    end, { desc = "Toggle lazygit (via toggleterm)" })
+    end, { desc = "Toggle lazygit" })
   end,
 }
